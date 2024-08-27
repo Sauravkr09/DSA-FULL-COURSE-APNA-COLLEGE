1ee94b08ae95eb40054395b3b2695273552a518e
Delta Student <souravkr9012@gmail.com>
2024-08-27 23:43:38 +0530

    your message

--- ArrayList/Pair1.java
+++ ArrayList/Pair1.java
@@ -1,17 +1,41 @@
 import java.util.*;
 public class Pair1 {
 
-    public static boolean pairSum1(ArrayList<Integer> list, int target){
+  //   public static boolean pairSum1(ArrayList<Integer> list, int target){
 
-  for(int i=0; i<list.size(); i++){
-    for(int j=i+1; j<list.size(); j++){
-        if(list.get(i) + list.get(j) == target){
-            return true;
-        }
+  // for(int i=0; i<list.size(); i++){
+  //   for(int j=i+1; j<list.size(); j++){
+  //       if(list.get(i) + list.get(j) == target){
+  //           return true;
+  //       }
+  //   }
+  // }
+  // return false;
+  //   }
+
+  // 2pointer approach
+    
+  public static boolean  pairSum1(ArrayList<Integer> list, int target){
+    int lp =0;
+    int rp = list.size()-1;
+     
+    while(lp != rp){
+      //case 1
+      if(list.get(lp)+list.get(rp) == target){
+        return true;
+      }
+
+      //case 2
+      if(list.get(lp)+list.get(rp) < target){
+        lp++;
+      }else{
+        //case 3
+        rp --;
+      }
+
     }
+return false;
   }
-  return false;
-    }
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         //1,2,3,4,5,6
