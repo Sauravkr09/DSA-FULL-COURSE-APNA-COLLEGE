c4aff32ae609d277391d4708237402244680d7a9
Delta Student <souravkr9012@gmail.com>
2024-09-04 21:55:38 +0530

    your message

---
+++ Satck/Q1.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class Q1 {
+    // public static void pushAtBottom(Stack<Integer> s, int data) {
+    //     if (s.isEmpty()) {
+    //         s.push(data);
+    //         return;
+    //     }
+    //     int top = s.pop();
+    //     pushAtBottom(s, data);
+    //     s.push(top);
+
+    // }
+    public static String reverseString(String str) {
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while (idx < str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!s.isEmpty()) {  // Corrected to s.isEmpty()
+            char curr = s.pop();  // pop() returns a char here
+            result.append(curr);
+        }
+
+        return result.toString();
+    }
+
+    //Reverse a stack
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+
+    }
+
+public static void reverseStack(Stack<Integer> s){
+    if(s.isEmpty()){
+        return;
+    }
+
+    int top = s.pop();
+    reverseStack(s);
+    pushAtBottom(s,top);
+}
+
+public static void printStack(Stack<Integer> s){
+    while(!s.isEmpty()){
+        System.out.println(s.pop());
+    }
+}
+
+    public static void main(String[] args) {
+       Stack<Integer> s = new Stack<>();
+       s.push(1);
+       s.push(2);
+       s.push(3);
+
+      
+       reverseStack(s);
+       printStack(s);
+
+    }
+}
+
